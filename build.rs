1832976f410edaddb69b483b97b338dfafbe027d
XuHugo <xq-310@163.com>
2023-10-12 14:01:35 +0800

    add protobuf codegen
    Signed-off-by: XuHugo <xq-310@163.com>

--- build.rs
+++ build.rs
@@ -4,5 +4,12 @@ fn main() {
     build_data::set_GIT_DIRTY();
     build_data::set_SOURCE_TIMESTAMP();
     build_data::no_debug_rebuilds();
+
+    protobuf_codegen_pure::Codegen::new()
+    .out_dir("protos/src")
+    .inputs(["protos/src/common.proto", "protos/src/ledger.proto", "protos/src/consensus.proto"])
+    .include("protos/src")
+    .run()
+    .expect("protobuf codegen failed!!!");
 }
 
